eb675424f42f9afdb5071d593cb76afba831975f
“[firstname <github0921@gmail.com>
2025-08-02 22:41:09 +0300

    Ajust large  desktop view undo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${protestStrike.variable} ${poppins.variable} antialiased bg-background max-w-[1920px] mx-auto`}>
+      <body className={`${protestStrike.variable} ${poppins.variable} antialiased bg-background`}>
         {children}
       </body>
     </html>
