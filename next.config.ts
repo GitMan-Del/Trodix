656571d0a0084e8a0326ea2cac0074d353759d70
“[firstname <github0921@gmail.com>
2025-08-07 22:47:15 +0300

    Profile User Avatar/2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "lh1.googleusercontent.com",
+      "lh2.googleusercontent.com",
+      "lh3.googleusercontent.com",
+      "lh4.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "lh6.googleusercontent.com",
+    ],
+  },
 };
 
 export default nextConfig;
